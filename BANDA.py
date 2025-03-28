fae752102ed3a24fe9663cde05495aeb831685c8
BANDA-KING <b.k.boukers@gmail.com>
2025-03-28 23:10:40 +0100

    Update BANDA.py

--- BANDA.py
+++ BANDA.py
@@ -1,9 +1,3 @@
-#!/data/data/com.termux/files/usr/bin/python
-import platform,os
-#####
-os.system("git pull")
-bit = platform.architecture()[0]
-if bit == '64bit':
-    import BANDA
-elif bit == '32bit':
-    import BANDA
+import BANDA
+from BANDA import Main
+Main.checkin_neon()
