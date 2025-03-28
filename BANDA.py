b3c95a9b622f50e61e3f1f3c056f9d2c617a30e7
BANDA-KING <b.k.boukers@gmail.com>
2025-03-28 23:21:56 +0100

    run.py

--- BANDA.py
+++ BANDA.py
@@ -1,3 +1,9 @@
-import os
-os.system('git pull')
-import BANDA
+#!/data/data/com.termux/files/usr/bin/python
+import platform,os
+#####
+os.system("git pull")
+bit = platform.architecture()[0]
+if bit == '64bit':
+    import BANDA
+elif bit == '32bit':
+    import BANDA
